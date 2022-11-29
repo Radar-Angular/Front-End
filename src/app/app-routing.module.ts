73ea1ed011db0ca4e40b8abddb40a66c44e47110
Moyses Santana Thomaz <moyses.th@gmail.com>
2022-11-28 22:43:17 -0300

    att rotas e modulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-{
-  path:'login',
-  loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
-  
-}
-
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { PedidoFormularioComponent } from './pedidos/pedido-formulario/pedido-fo
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { HomeComponent } from './home/home.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -27,7 +27,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     PedidoFormularioComponent,
     FooterComponent,
     HeaderComponent,
-   HomeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
