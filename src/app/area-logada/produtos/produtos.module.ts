ad49aa371ec5e8703261042574e194df7b3d6f21
Matheus <matheusgaldinoinfo@gmail.com>
2022-11-30 17:14:02 -0300

    Criada a model de Produtos e a service de produtos com Crud Completo

--- src/app/area-logada/produtos/produtos.module.ts
+++ src/app/area-logada/produtos/produtos.module.ts
@@ -5,6 +5,7 @@ import { ProdutosRoutingModule } from './produtos-routing.module';
 import { ProdutoFormularioComponent } from './produto-formulario/produto-formulario.component';
 import { ProdutoListaComponent } from './produto-lista/produto-lista.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ProdutosRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class ProdutosModule { }
--- src/app/shared/services/produto.service.ts
+++ src/app/shared/services/produto.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, firstValueFrom, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Produto } from '../models/produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+    public async  getProdutos() : Promise<Produto[]>{
+        let produtos:Produto[] = await firstValueFrom(this.http.get<Produto[]>(`${this.apiUrl}/produtos`))
+        return produtos
+      }
+
+      public async getProdutoById(id:number) : Promise<Produto>{
+        return await firstValueFrom(this.http.get<Produto>(`${this.apiUrl}/produto/${id}`))
+      }
+
+      public async getProdutoByNome(nome: string) : Promise<Produto>{
+        return await firstValueFrom(this.http.get<Produto>(`${this.apiUrl}/produtos/${nome}`))
+      }
+
+      public async postProduto(produto: Produto): Promise<Produto>{
+        let produtoRest:Produto = await firstValueFrom(this.http.post<Produto>(`${this.apiUrl}/produto`,produto))
+        return produtoRest
+      }
+
+      public async putProduto(produto: Produto): Promise<Produto>{
+        let produtoRest:Produto = await firstValueFrom(this.http.post<Produto>(`${this.apiUrl}/produto/${produto.id}`, produto))
+        return produtoRest
+      }
+
+
+      public deleteClienteById(id:number){
+        firstValueFrom(this.http.delete(`${this.apiUrl}/produto/${id}`))
+      }
+
 }
