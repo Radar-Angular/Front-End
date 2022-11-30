8aa552ad1832a4351224185c6bdde141b3d01f78
Moyses Thomaz <moyses.th@gmail.com>
2022-11-30 19:13:20 -0300

    funcionalidade botao logout

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -8,62 +8,63 @@ import { Usuario } from '../../login/model/usuario';
 export class AuthService {
 
   usuario?: Usuario;
-  token?: string 
+  token?: string
 
 
   constructor(
-    private router:Router
+    private router: Router
   ) { }
 
 
-setUsuario(usuario: Usuario){
-  this.usuario = usuario;
-  sessionStorage.setItem('usuario', JSON.stringify(usuario))
-  console.log("Caiu aq?")
-}
+  setUsuario(usuario: Usuario) {
+    this.usuario = usuario;
+    sessionStorage.setItem('usuario', JSON.stringify(usuario))
+    console.log("Caiu aq?")
+  }
 
-getUsuario(){
-    if(this.usuario) return this.usuario
+  getUsuario() {
+    if (this.usuario) return this.usuario
 
 
     const usuarioGuardado = sessionStorage.getItem('usuario')
 
-    if(usuarioGuardado){
+    if (usuarioGuardado) {
       this.usuario = JSON.parse(usuarioGuardado)
       return this.usuario
     }
-return null;
-}
-
-setToken(token: string){
-  this.token = token
-
-  sessionStorage.setItem('token', token)
-}
-
-getToken(){
-  if(this.token) return this.token
-
-  const tokenGuardado = sessionStorage.getItem('token')
-
-  if (tokenGuardado){
-    this.token = tokenGuardado
-    return this.token
+    return null;
   }
-  return null
-}
 
-estaLogado(): boolean{
-  if(this.getToken() && this.getToken()){
-    return true
+  setToken(token: string) {
+    this.token = token
+
+    sessionStorage.setItem('token', token)
   }
-  return false
-}
 
-logout(){
-  this.usuario = undefined;
-  this.token = '';
-  localStorage.clear();
-  this.router.navigate(['login'])
-}
+  getToken() {
+    if (this.token) return this.token
+
+    const tokenGuardado = sessionStorage.getItem('token')
+
+    if (tokenGuardado) {
+      this.token = tokenGuardado
+      return this.token
+    }
+    return null
+  }
+
+  estaLogado(): boolean {
+    if (this.getToken() && this.getToken()) {
+      return true
+    }
+    return false
+  }
+
+  logout() {
+    this.usuario = undefined;
+    this.token = '';
+    sessionStorage.clear();
+    this.router.navigate(['login'])
+    console.log('deu logout')
+  }
 }
